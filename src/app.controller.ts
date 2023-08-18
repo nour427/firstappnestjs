51a513a2b429629812f6d4bc38f5d38914913380
nour427 <nour.chaaben@insat.ucar.tn>
2023-08-18 11:26:17 +0100

    last changes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,15 @@
 import { AppService } from './app.service';
 import { Controller, Post, Body, Get} from '@nestjs/common';
 import { CreateIntentDto } from './CreateIntentDto';
+import { Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { StripeModule } from 'nestjs-stripe';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  stripe: any;
+  constructor(private readonly appService: AppService) {
 
+  }
   @Get('hello')
   getHello() {
     return "Hello";
@@ -16,7 +21,48 @@ export class AppController {
       const intent = await this.appService.createIntent(amount, currency);
       return { clientSecret: intent.client_secret };
     }
-  
+
+
+  //webhooks
+  @Post('webhook')
+  async handleStripeWebhook(@Req() req: Request, @Res() res: Response) {
+    const sig = req.headers['stripe-signature'];
+
+    try {
+      const event = this.stripe.webhooks.constructEvent(
+        req.body,
+        sig,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+
+      // Handle the Stripe event here
+      switch (event.type) {
+        case 'payment_intent.succeeded':
+          // Handle successful payment
+          const paymentIntent = event.data.object;
+          break;
+          case 'payment_intent.payment_failed':
+            // Handle failed payment
+            break;
+          case 'payment_method.attached':
+            const paymentMethod = event.data.object;
+            const customerId = paymentMethod.customer;
+
+      
+          // Handle failed payment
+          break;
+        // Add more cases for other event types
+        default:
+          console.log(`Unhandled event type: ${event.type}`);
+      }
+
+      res.status(200).send();
+
+    } catch (err) {
+      console.error(err);
+      res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+  }
   
   }
 // Replace CreateIntentDto with a DTO (Data Transfer Object) representing the request body for creating an intent
--- src/app.service.ts
+++ src/app.service.ts
@@ -18,14 +18,20 @@ export class AppService {
     this.stripe = stripeClient;
 
   }
-
+ 
+  
   async createIntent(amount: number, currency: string): Promise<Stripe.PaymentIntent> {
     try {
       const intent = await this.stripe.paymentIntents.create({
         amount,
         currency,
+       /* automatic_payment_methods: {
+          enabled: true,
+        },*/
         //Todo should add "apple_pay"
-        payment_method_types: ["card","paypal"]
+        payment_method_types: ["card","paypal"],
+       
+
         
       });
       
@@ -36,7 +42,23 @@ export class AppService {
       throw new Error('Failed to create payment intent.');
     }
   }
+  //webhooks
+  async function (){
+    const endpoint = await this.stripe.webhookEndpoints.create({
+      url: 'https://example.com/my/webhook/endpoint',
+      enabled_events: [
+        'payment_intent.payment_failed',
+        'payment_intent.succeeded',
+        'payment_method.attached',
+      ],
+    });
+  }
+  
 }
+
+
+
+
 
 
 
